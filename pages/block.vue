7d3e0fb9bebff952236ceb5db76143ddbbd9d693
xpgeng <xpgeng.me@gmail.com>
2020-04-01 17:19:37 +0800

    chore: move navbar to default layout

--- pages/block.vue
+++ pages/block.vue
@@ -1,64 +1,47 @@
 <template>
-  <el-container >
-    <el-header>
-      <!-- <img src="../assets/logo.png" alt="logo" class="head-log"> -->
-      <el-menu :default-active="$route.path" class="el-menu" mode="horizontal">
-        <el-menu-item index="/index">EXPLORER</el-menu-item>
-        <Search />
-      </el-menu>
-    </el-header>
+  <el-container>
+    <el-row>
+      <el-col :span="8" >
+        Height
+        <p >{{block.height}}</p>
+      </el-col>
+      <el-col :span="8" >
+        ID
+        <p >{{block.id}}</p>
+      </el-col>
+      <el-col :span="8" >
+        Date
+        <p >{{block.timestamp}}</p>
+      </el-col>
+    </el-row>
 
-    <el-main>
-
-      <el-row>
-        <el-col :span="8" >
-          Height
-          <p >{{block.height}}</p>
-        </el-col>
-        <el-col :span="8" >
-          ID
-          <p >{{block.id}}</p>
-        </el-col>
-        <el-col :span="8" >
-          Date
-          <p >{{block.timestamp}}</p>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="8" >
-          Transaction count
-          <p >{{block.count}}</p>
-        </el-col>
-        <el-col :span="8" >
-          Transaction Fees
-          <p >{{block.fees}}</p>
-        </el-col>
-        <el-col :span="8" >
-          Reward
-          <p >{{block.reward}}</p>
-        </el-col>
-      </el-row>
-      <h2>Transactions included in this block</h2>
-      <el-table :data="transactions" stripe style="width: 80%; margin: auto;">
-        <el-table-column prop="height" align="center" label="Height" width="200"></el-table-column>
-        <el-table-column prop="id" align="center" label="Block ID" width="300"></el-table-column>
-        <el-table-column prop="timestamp" align="center" label="Forged Time" width="180"></el-table-column>
-        <!-- <el-table-column prop="fees" align="center" label="Fee"></el-table-column> -->
-        <el-table-column prop="delegate" align="center" label="Delegate"></el-table-column>
-      </el-table>
-
-    </el-main>
-
-    <el-footer>
-
-    </el-footer>
+    <el-row>
+      <el-col :span="8" >
+        Transaction count
+        <p >{{block.count}}</p>
+      </el-col>
+      <el-col :span="8" >
+        Transaction Fees
+        <p >{{block.fees}}</p>
+      </el-col>
+      <el-col :span="8" >
+        Reward
+        <p >{{block.reward}}</p>
+      </el-col>
+    </el-row>
+    <h2>Transactions included in this block</h2>
+    
+    <el-table :data="transactions" stripe style="width: 80%; margin: auto;">
+      <el-table-column prop="height" align="center" label="Height" width="200"></el-table-column>
+      <el-table-column prop="id" align="center" label="Block ID" width="300"></el-table-column>
+      <el-table-column prop="timestamp" align="center" label="Forged Time" width="180"></el-table-column>
+      <!-- <el-table-column prop="fees" align="center" label="Fee"></el-table-column> -->
+      <el-table-column prop="delegate" align="center" label="Delegate"></el-table-column>
+    </el-table>
   </el-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue';
-import Search from '~/components/Search.vue';
 import moment from 'moment';
 import * as gnyClient from '@gny/client';
 const connection = new gnyClient.Connection(
@@ -70,11 +53,6 @@ const connection = new gnyClient.Connection(
 
 
 export default {
-  components: {
-    Logo,
-    Search,
-  },
-
   data() {
     return {
       block: {},
@@ -93,22 +71,5 @@ export default {
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,32 @@
 <template>
-  <el-container >
-    <el-header>
-      <!-- <img src="../assets/logo.png" alt="logo" class="head-log"> -->
-      <el-menu :default-active="$route.path" class="el-menu" mode="horizontal">
-        <el-menu-item index="/index">EXPLORER</el-menu-item>
-        <Search />
-      </el-menu>
-    </el-header>
-    <el-main>
-      <Basic />
+  <el-container direction="vertical">
+    <Basic />
 
-      <Block />
-
-    </el-main>
-    <el-footer>
-
-    </el-footer>
+    <Block />
   </el-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue';
 import Basic from '~/components/Basic.vue';
 import Block from '~/components/Block.vue';
-import Search from '~/components/Search.vue';
+
 
 export default {
   components: {
-    Logo,
     Basic,
-    Block,
-    Search
+    Block
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+.el-container {
+  max-width: 1000px;
+  box-sizing: border-box;
+  margin: 0px auto;
+  padding: 20px 20px;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+
+
 </style>
