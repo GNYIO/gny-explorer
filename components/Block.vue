1a69b83116ea0d7c066ba79eab13ffa74f2eda33
Matthias Knopp <matthias-knopp@gmx.net>
2020-05-06 12:23:42 +0200

    chore: fix GNY_HTTPS env variable

--- components/Block.vue
+++ components/Block.vue
@@ -41,7 +41,7 @@ const connection = new gnyClient.Connection(
   process.env['GNY_ENDPOINT'],
   process.env['GNY_PORT'],
   process.env['GNY_NETWORK'],
-  process.env['GNY_HTTPS'],
+  process.env['GNY_HTTPS'] || false,
 );
 
 export default {
