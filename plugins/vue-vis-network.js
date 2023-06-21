bfcb5ab3004d075e897dd2422ede478e6e5d45d3
Matthias Knopp <matthias-knopp@gmx.net>
2023-06-21 13:47:45 +0200

    chore: remove unused dependencies
    removed vis-data
    
    removed vis-network
    
    removed vis-util

--- plugins/vue-vis-network.js
+++ plugins/vue-vis-network.js
@@ -1,6 +1,4 @@
 import Vue from 'vue'
 
-import { Network,Timeline, Graph2d, } from "vue-visjs";
+import { Network } from "vue-visjs";
 Vue.component('network', Network);
-Vue.component('timeline', Timeline);
-Vue.component('graph2d', Graph2d);
