b3d65d16328be622d0a4bbf5d2d62a256d89e6b2
Matthias Knopp <matthias-knopp@gmx.net>
2023-09-03 11:09:10 +0200

    chore: show total NFTs and NFT Makers

--- pages/nfts.vue
+++ pages/nfts.vue
@@ -1,92 +1,74 @@
 <template>
-  <b-card title="NTFs" class="shadow">
-    <el-table :data="currentNfts" stripe height="500" v-loading="loading">
-      
-      <el-table-column
-        v-if="width >= 550"
-        prop="name"
-        align="center"
-        label="Name"
-        width="auto">
-      <template v-slot:default="table">
-          <nuxt-link class="nuxt-link" :to="{ name: 'nft-detail', query: { name: table.row.name } }">
-            {{ table.row.name }}
-          </nuxt-link>
-        </template>
-    </el-table-column>
+  <div>
+    <b-card title="NFTs Overview" class="shadow">
+      <div class="wrapper">
+        <div>
+          NFTs
+          <p>{{ totalNfts }}</p>
+        </div>
+        <div>
+          NFT Makers
+          <p>{{ totalNftMakers }}</p>
+        </div>
+      </div>
+    </b-card>
 
-      <el-table-column
-        prop="hash"
-        align="center"
-        label="Hash"
-        width="auto">
-        <template v-slot:default="table">
-          <nuxt-link class="nuxt-link" :to="{ name: 'nft-detail', query: { hash: table.row.hash } }">
-            {{ table.row.hash.slice(0, 16) }}
-          </nuxt-link>
-        </template>
-      </el-table-column>
+    <b-card title="All NFTs" class="shadow">
+      <el-table :data="nfts" stripe height="300" v-loading="loading">
+        <el-table-column v-if="width >= 550" prop="name" align="center" label="Nft Name" width="auto">
+          <template v-slot:default="table">
+            <nuxt-link class="nuxt-link" :to="{ name: 'nft-detail', query: { name: table.row.name } }">
+              {{ table.row.name | truncate(8) }}
+            </nuxt-link>
+          </template>
+        </el-table-column>
+        <el-table-column prop="hash" align="center" label="Nft Hash" width="auto">
+          <template v-slot:default="table">
+            <nuxt-link class="nuxt-link" :to="{ name: 'nft-detail', query: { hash: table.row.hash } }">
+              {{ table.row.hash | truncate(8) }}
+            </nuxt-link>
+          </template>
+        </el-table-column>
+        <el-table-column prop="previousHash" align="center" label="Previous Hash" width="auto">
+          <template v-slot:default="table">
+            <nuxt-link v-if="table.row.previousHash !== null" class="nuxt-link"
+              :to="{ name: 'nft-detail', query: { hash: table.row.previousHash } }">
+              {{ table.row.previousHash | truncate(8) }}
+            </nuxt-link>
+            <span v-else>no hash</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="tid" align="center" label="Transaction ID" width="auto">
+          <template v-slot:default="table">
+            <nuxt-link class="nuxt-link" :to="{ name: 'transaction-detail', query: { id: table.row.tid } }">
+              {{ table.row.tid | truncate(8) }}
+            </nuxt-link>
+          </template>
+        </el-table-column>
+        <el-table-column prop="ownerAddress" align="center" label="Owner Address" width="auto">
+          <template v-slot:default="table">
+            <nuxt-link class="nuxt-link" :to="{ name: 'account-detail', query: { address: table.row.ownerAddress } }">
+              {{ table.row.ownerAddress | truncate(6) }}
+            </nuxt-link>
+          </template>
+        </el-table-column>
+        <el-table-column prop="nftMakerId" align="center" label="Nft Maker" width="auto">
+          <template v-slot:default="table">
+            <nuxt-link class="nuxt-link" :to="{ name: 'nft-maker-detail', query: { makerId: table.row.nftMakerId } }">
+              {{ table.row.nftMakerId | truncate(8) }}
+            </nuxt-link>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination @current-change="handleCurrentChange" :current-page="currentNftPage" :page-size="5"
+        layout="prev, pager, next" :total="nftsCount" align="center"></el-pagination>
+    </b-card>
 
-      <el-table-column
-        prop="previousHash"
-        align="center"
-        label="Previous Hash"
-        width="auto">
-        <template v-slot:default="table">
-          <nuxt-link v-if="table.row.previousHash !== null" class="nuxt-link" :to="{ name: 'nft-detail', query: { hash: table.row.previousHash } }">
-            {{ table.row.previousHash }}
-          </nuxt-link>
-          <span v-else>no hash</span>
-        </template>
-      </el-table-column>
+    <b-card title="All NFT Makers" class="shadow">
 
-      <el-table-column
-        prop="tid"
-        align="center"
-        label="Transaction ID"
-        width="auto">
-        <template v-slot:default="table">
-          <nuxt-link class="nuxt-link" :to="{ name: 'transaction-detail', query: { id: table.row.tid } }">
-            {{ table.row.tid.slice(0, 8) }}
-          </nuxt-link>
-        </template>
-      </el-table-column>
+    </b-card>
 
-      <el-table-column
-        prop="ownerAddress"
-        align="center"
-        label="Owner Address"
-        width="auto">
-        <template v-slot:default="table">
-          <nuxt-link class="nuxt-link" :to="{ name: 'account-detail', query: { address: table.row.ownerAddress } }">
-            {{ table.row.ownerAddress.slice(0, 8) }}
-          </nuxt-link>
-        </template>
-      </el-table-column>
-
-      <el-table-column
-        prop="nftMakerId"
-        align="center"
-        label="Nft Maker"
-        width="auto">
-        <template v-slot:default="table">
-          <nuxt-link class="nuxt-link" :to="{ name: 'nft-maker-detail', query: { makerId: table.row.nftMakerId } }">
-            {{ table.row.nftMakerId }}
-          </nuxt-link>
-        </template>
-      </el-table-column>
-
-
-    </el-table>
-    <el-pagination
-      @current-change="handleCurrentChange"
-      :current-page="currentPage"
-      :page-size="10"
-      layout="prev, pager, next"
-      :total="nftsCount"
-      align="center"
-    ></el-pagination>
-  </b-card>
+  </div>
 </template>
 
 <script>
@@ -106,49 +88,78 @@ export default {
   },
   data() {
     return {
+      totalNfts: 0,
+      totalNftMakers: 0,
+
       nfts: [],
       nftsCount: 0,
-      currentNfts: [],
-      currentPage: 1,
-      pageSize: 10,
-      loaded: 0,
+      currentNftPage: 1,
+      nftPageSize: 5,
       loading: true
     }
   },
   methods: {
-    handleCurrentChange(currentPage) {
-      this.currentPage = currentPage
-      this.changePage(this.nfts, currentPage)
-    },
+    async handleCurrentChange(currentNftPage) {
+      this.loading = true;
+      console.log(`(Nfts) load data for page "${currentNftPage}"`);
 
-    changePage(list, currentPage) {
-      let from = (currentPage - 1) * this.pageSize
-      let to = currentPage * this.pageSize
-      this.currentNfts = []
-      for (; from < to; from++) {
-        if (list[from]) {
-          this.currentNfts.push(list[from])
-        }
-      }
-    }
+      const from = (currentNftPage - 1) * this.nftPageSize;
+      const offset = from;
+      const limit = this.nftPageSize;
+
+      const { nfts, count } = await connection.api.Nft.getNfts(offset, limit);
+      this.nfts = nfts;
+      this.nftsCount = count;
+
+      console.log(JSON.stringify(nfts, null, 2));
+
+      this.loading = false;
+    },
   },
 
   async mounted() {
-    const { nfts } = await connection.api.Nft.getNfts();
-    this.nfts = nfts;
+    const totalNfts = await connection.api.Nft.getNfts();
+    const totalNftMakers = await connection.api.Nft.getNftMakers();
+    this.totalNfts = totalNfts.count;
+    this.totalNftMakers = totalNftMakers.count;
 
-    this.nftsCount = this.nfts.length
-    this.handleCurrentChange(1);
 
-    if (this.nfts.length >= 0) {
-      this.loading = false
-    }
+    // reset
+    this.nfts = [];
+    this.nftsCount = 0;
+
+    await this.handleCurrentChange(1);
   }
 }
 </script>
 
 <style scoped>
 .card {
-  margin-bottom: 2rem; 
+  margin-bottom: 2rem;
+}
+
+.wrapper {
+  margin: 0;
+  display: grid;
+  grid-template-columns: 1fr;
+  align-items: stretch;
+}
+
+@media screen and (min-width: 300px) {
+  .wrapper {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+
+@media screen and (min-width: 500px) {
+  .wrapper {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+}
+
+@media screen and (min-width: 800px) {
+  .wrapper {
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  }
 }
 </style>
